8d5c5134c15b86abe31e5e8580977a89d38a50a0
Sak32009 <Sak32009@users.noreply.github.com>
2024-08-18 14:23:53 +0200

    added helper generate_credits.bat

---
+++ generate_credits.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal EnableDelayedExpansion
+cd /d "%~dp0"
+
+set "CREDITS_FILE=CREDITS.md"
+
+if exist "%CREDITS_FILE%" (
+  del /f /s /q "%CREDITS_FILE%"
+)
+
+set "GLOB=third-party libs tools\steamclient_loader"
+set "FILTER=SOURCE.txt"
+
+echo:# Many thanks for these sources>> "%CREDITS_FILE%"
+
+for %%A in (%GLOB%) do (
+  powershell -Command "Get-ChildItem -LiteralPath \"%%~A\" -Filter \"%FILTER%\" -File -Recurse | foreach { $parent = Split-Path -Path $_.FullName -Parent; $relative = (Resolve-Path -Path $parent -Relative).replace(\".\\\",\"\"); Write-Output \"- ^[$^($relative^)^]^(#$^($relative^)^)\"; }">> "%CREDITS_FILE%"
+)
+
+echo.>> "%CREDITS_FILE%"
+
+for %%B in (%GLOB%) do (
+  powershell -Command "Get-ChildItem -LiteralPath \"%%~B\" -Filter \"%FILTER%\" -File -Recurse | foreach { $parent = Split-Path -Path $_.FullName -Parent; $relative = (Resolve-Path -Path $parent -Relative).replace(\".\\\",\"\"); Write-Output \"### $^($relative^)\"; Write-Output \"\"; Get-Content -LiteralPath $_.FullName -Raw -Encoding utf8; }">> "%CREDITS_FILE%"
+)
+
+endlocal
