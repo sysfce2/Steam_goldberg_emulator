21c33b3a98c678da5cfb2002bc7f1ad4d6e6d6ef
alex47exe <17827464+alex47exe@users.noreply.github.com>
2024-10-14 15:23:21 +0100

    generate_emu_config and migrate_gse: copy _DEFAULT folders for Linux too

--- tools/migrate_gse/rebuild_linux.sh
+++ tools/migrate_gse/rebuild_linux.sh
@@ -18,6 +18,9 @@ source "./$venv/bin/activate"
 echo building migrate_gse...
 pyinstaller "main.py" --distpath "$out_dir" -y --clean --onedir --name "migrate_gse" --noupx --console -i "NONE" --workpath "$build_temp_dir" --specpath "$build_temp_dir" || exit 1
 
+#cp -a "post_build" "%out_dir%/generate_emu_config/post_build" # skip this, as it's only for Windows; will have to use a different 'post_build' folder for Linux
+cp -a "_DEFAULT" "%out_dir%/generate_emu_config/_DEFAULT"
+
 cp -f "README.md" "$out_dir/migrate_gse"
 
 echo;
